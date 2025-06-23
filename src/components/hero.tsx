3ad9a8e12388fef0cf9bbfb72fa92b05cfee7632
edsonarios <edsonrios9@gmail.com>
2025-06-22 21:56:17 -0400

    Fix

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ const URL = import.meta.env.VITE_MEDIA_URL || "/media/";
 
 export default function Hero() {
   return (
-    <section className="relative h-[600px] mt-10">
+    <section className="relative h-[600px] mt-24 md:mt-10">
       <img
         src={`${URL}f-franz.webp`}
         alt="Trabajador instalando gas"
@@ -29,18 +29,18 @@ export default function Hero() {
             reparación de sistemas de gas domiciliario en toda Bolivia. Más de
             10 años de experiencia garantizando tu seguridad.
           </p>
-          <div className="flex gap-6 flex-wrap" data-aos="fade-right">
-            <div className="bg-white/80 backdrop-blur p-2 rounded-xl shadow-md text-center w-40">
+          <div className="flex gap-2 flex-wrap" data-aos="fade-right">
+            <div className="bg-white/80 backdrop-blur p-1 rounded-xl shadow-md text-center w-40">
               <Users className="size-10 text-green-600 mx-auto mb-2" />
               <div className="text-3xl font-bold text-gray-800">100+</div>
               <div className="text-gray-600 text-sm">Clientes Satisfechos</div>
             </div>
-            <div className="bg-white/80 backdrop-blur p-2 rounded-xl shadow-md text-center w-40">
+            <div className="bg-white/80 backdrop-blur p-1 rounded-xl shadow-md text-center w-40">
               <Award className="size-10 text-green-600 mx-auto mb-2" />
               <div className="text-3xl font-bold text-gray-800">10+</div>
               <div className="text-gray-600 text-sm">Años de Experiencia</div>
             </div>
-            <div className="bg-white p-2 rounded-xl shadow-lg w-40 text-center">
+            <div className="bg-white p-1 rounded-xl shadow-lg w-40 text-center">
               <Star className="size-10 text-green-600 mx-auto mb-2" />
               <div className="text-3xl font-bold text-gray-800">100%</div>
               <div className="text-gray-600 text-sm">Trabajos Garantizados</div>
